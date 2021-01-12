e721ffa1b97da771557069f44191931bc143a451
guypwhunt <guypwhunt1992@gmail.com>
2021-01-12 18:39:24 +0000

    Data Visulisations Added

--- app.R
+++ app.R
@@ -3,6 +3,7 @@
   library(GEOquery)
   library(limma)
   library(umap)
+  library("maptools")
   
   ui <- fluidPage(
     titlePanel("GEO2R Data Visualisation"),
@@ -11,7 +12,14 @@
     textInput("geo_accession_code", "GEO accession code", "GSE18384"),
     helpText("Please enter your platform of interest, if the Series is associated with multiple platforms."),
     textInput("platform", "Platform", "GPL6246"),
-    mainPanel(dataTableOutput('myTable'))
+    mainPanel(tabsetPanel(type = "tabs",
+                          tabPanel("Options", plotOutput("plot")),
+                          tabPanel("Dataset", dataTableOutput('myTable')),
+                          tabPanel("Box-and-Whisper Plot", plotOutput('boxPlot')),
+                          tabPanel("Expression Density Plot", plotOutput('expressionDensity')),
+                          tabPanel("Mean-Variance Plot", plotOutput('meanVariance')),
+                          tabPanel("UMAP Plot", plotOutput('umap'))
+    ))
   )
   
   server <- function(input, output, session){
@@ -28,8 +36,42 @@
     ex <- log2(ex) }
     return(ex)})
     
+    gset <- reactive({gset <- getGEO(input$geo_accession_code, GSEMatrix =TRUE, getGPL=FALSE)
+    if (length(gset) > 1) idx <- grep(input$platform, attr(gset, "names")) else idx <- 1
+    gset <- gset[[idx]]
+    return(gset)})
+    
+    # Data Set Plot
     output$myTable <- renderDataTable({
       data_input()
+    })
+    
+    # Box-and-Whisker Plot
+    output$boxPlot <- renderPlot({
+      par(mar=c(7,4,2,1))
+      title <- paste (input$geo_accession_code, "/", input$platform, sep ="")
+      boxplot(data_input(), boxwex=0.7, notch=T, main=title, outline=FALSE, las=2)
+    })
+    
+    # Expression Value Distribution Plot
+    output$expressionDensity <- renderPlot({
+    par(mar=c(4,4,2,1))
+    title <- paste (input$geo_accession_code, "/", input$platform, " value distribution", sep ="")
+    plotDensities(data_input(), main=title, legend=F)
+    })
+    
+    # Mean-Variance Plot
+    output$meanVariance <- renderPlot({
+    ex <- na.omit(data_input()) # eliminate rows with NAs
+    plotSA(lmFit(ex), main= paste("Mean variance trend,", input$geo_accession_code))
+    })
+    
+    # UMAP plot (multi-dimensional scaling)
+    output$umap <- renderPlot({
+    ex <- data_input()[!duplicated(data_input()), ]  # remove duplicates
+    ump <- umap(t(ex), n_neighbors = 4, random_state = 123)
+    plot(ump$layout, main="UMAP plot, nbrs=4", xlab="", ylab="", pch=20, cex=1.5)
+    pointLabel(ump$layout, labels = rownames(ump$layout), method="SANN", cex=0.6)
     })
   }
   
