9e5f5e0a02cf36236ffc18a3b3af398d82eb6185
guypwhunt <guypwhunt1992@gmail.com>
2021-01-19 19:34:08 +0000

    Modular Updates

--- app.R
+++ app.R
@@ -5,13 +5,15 @@
   library(umap)
   library("maptools")
   library(ggplot2)
+  source("geoIntegration.R")
+  source("geo2rDataVisualisation.R")
   
   ui <- fluidPage(
     titlePanel("GEO2R Data Visualisation"),
     helpText("GEO2R is an interactive web tool that allows users to compare two or more groups of Samples in a GEO Series in order to identify genes that are differentially expressed across experimental conditions. GEO2R Data Visualisation extends GEO2R's functionalities by enabling a richer set of graphics to be generated from the GEO2R outputs."),
     sidebarPanel(
       helpText("Input a GEO accession code to examine the gene expression data."),
-      textInput("geo_accession_code", "GEO accession code", "GSE18384"),
+      textInput("geoAccessionCode", "GEO accession code", "GSE18384"),
       helpText("Input the platform of interest, if the series is associated with multiple platforms."),
       textInput("platform", "Platform", "GPL6246"),
       selectInput("pValueAdjustment", "Apply adjustment to the P-values:",
@@ -56,70 +58,36 @@
   
   server <- function(input, output, session){
     
-    gsetData <- reactive({gset <- getGEO(input$geo_accession_code, GSEMatrix =TRUE, getGPL=FALSE)
-    if (length(gset) > 1) idx <- grep(input$platform, attr(gset, "names")) else idx <- 1
-    gset <- gset[[idx]]
-    return(gset)})
+    gsetData <- reactive({getGeoData(input$geoAccessionCode, input$platform)})
     
-    data_input <- reactive({gset <- gsetData()
-    if (input$logTransformation == "Auto-Detect"){
-    ex <- exprs(gset)
-    # log2 transform
-    qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
-    LogC <- (qx[5] > 100) ||
-      (qx[6]-qx[1] > 50 && qx[2] > 0)
-    if (LogC) { ex[which(ex <= 0)] <- NaN
-    ex <- log2(ex)}
-    return(ex)} 
-    # this does not work for some reason!!!
-    else if (input$logTransformation == "Yes") {
-    ex <- exprs(gset)
-    #I think the line below is an error in the original source code as it causes the data to become NaN!!!
-    #ex <- ex[which(ex <= 0)] <- NaN
-    ex <- log2(ex)
-    return(ex)}
-    else if (input$logTransformation == "No") {
-    ex <- exprs(gset)
-    return(ex)}
+    dataInput <- reactive({extractGeoData(gsetData(), input$logTransformation)
     })
     
-    gset <- reactive({gset <- getGEO(input$geo_accession_code, GSEMatrix =TRUE, getGPL=FALSE)
-    if (length(gset) > 1) idx <- grep(input$platform, attr(gset, "names")) else idx <- 1
-    gset <- gset[[idx]]
-    return(gset)})
     
     # Data Set Plot
     output$myTable <- renderDataTable({
-      data_input()
+      dataInput()
     })
     
     # Box-and-Whisker Plot
     output$boxPlot <- renderPlot({
-      par(mar=c(7,4,2,1))
-      title <- paste (input$geo_accession_code, "/", input$platform, sep ="")
-      boxplot(data_input(), boxwex=0.7, notch=T, main=title, outline=FALSE, las=2)
-    })
+      boxAndWhiskerPlot(input$geoAccessionCode, input$platform, dataInput())
+      })
     
     # Expression Value Distribution Plot
     output$expressionDensity <- renderPlot({
-    par(mar=c(4,4,2,1))
-    title <- paste (input$geo_accession_code, "/", input$platform, " value distribution", sep ="")
-    plotDensities(data_input(), main=title, legend=F)
+      expressionValueDistributionPlot(input$geoAccessionCode, input$platform, dataInput())
     })
     
     # Mean-Variance Plot
     output$meanVariance <- renderPlot({
-    ex <- na.omit(data_input()) # eliminate rows with NAs
-    plotSA(lmFit(ex), main= paste("Mean variance trend,", input$geo_accession_code))
+      meanVariancePlot(input$geoAccessionCode, input$platform, dataInput())
     })
     
     # UMAP plot (multi-dimensional scaling)
     output$umap <- renderPlot({
-    ex <- data_input()[!duplicated(data_input()), ]  # remove duplicates
-    ump <- umap(t(ex), n_neighbors = 4, random_state = 123)
-    plot(ump$layout, main="UMAP plot, nbrs=4", xlab="", ylab="", pch=20, cex=1.5)
-    pointLabel(ump$layout, labels = rownames(ump$layout), method="SANN", cex=0.6)
-    })
+      umapPlot(input$geoAccessionCode, input$platform, dataInput())
+      })
   }
   
   shinyApp(ui, server)
---
+++ geoIntegration.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(GEOquery)
+library(limma)
+
+getGeoData <- function(geoAccessionCode, platform) {
+  gset <- getGEO(geoAccessionCode, GSEMatrix =TRUE, getGPL=FALSE)
+  if (length(gset) > 1) idx <- grep(platform, attr(gset, "names")) else idx <- 1
+  gset <- gset[[idx]]
+  return(gset)
+}
+
+extractGeoData <- function(gset, logTransformation) {
+  # If log transformation is set to auto-detect
+  if (logTransformation == "Auto-Detect"){
+    ex <- exprs(gset)
+    # log2 transform
+    qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+    LogC <- (qx[5] > 100) ||
+      (qx[6]-qx[1] > 50 && qx[2] > 0)
+    if (LogC) { ex[which(ex <= 0)] <- NaN
+    ex <- log2(ex)}
+    return(ex)} 
+  
+  # If log transformation is set to yes
+  else if (logTransformation == "Yes") {
+    ex <- exprs(gset)
+    ex <- log2(ex)
+    return(ex)}
+  
+  # If log transformation is set to no
+  else if (logTransformation == "No") {
+    ex <- exprs(gset)
+    return(ex)
+  }
+  }
+  
