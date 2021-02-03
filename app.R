918becfdc289ad78eb71f085a6209a082a2286cb
guypwhunt <guypwhunt1992@gmail.com>
2021-02-03 17:57:04 +0000

    Interactive Updates

--- app.R
+++ app.R
@@ -149,16 +149,7 @@
     
     # Interactive Box-and-Whisker Plot
     output$interactiveBoxAndWhiskerPlot <- renderPlotly({
-      #interactiveBoxAndWhiskerPlot(knnDataInput())
-      data <- na.omit(knnDataInput())
-      data <- as.data.frame(data)
-      fig <- plot_ly(data, type = "box", quartilemethod="linear")
-      i = 1
-      for(col in names(data)) {
-        fig <- fig %>% add_trace(x = names(data)[i], y = data[,i], quartilemethod="linear", name=names(data)[i])
-        i <- i+1
-      }
-      fig
+      interactiveBoxAndWhiskerPlot(knnDataInput())
     })
     
     
