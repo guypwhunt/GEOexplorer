5f73e33ba5d24ef14ba373bf9111274ef53844cc
guypwhunt <guypwhunt1992@gmail.com>
2021-02-07 20:44:55 +0000

    Experiment Updates

--- experimentalFunctions/plotilyExperiment.R
+++ experimentalFunctions/plotilyExperiment.R
@@ -29,29 +29,5 @@
   library(plotly)
   library(dplyr)
   
-  lmFit(data)
-  
-  knnDataInput <- as.data.frame(knnDataInput)
-  
-  knnDataInput <- na.omit(knnDataInput)
-  ex <- data[!duplicated(knnDataInput), ]  # remove duplicates
-  nNeighbors <- 5
-  ump <- umap(t(ex), n_neighbors = nNeighbors, random_state = 123)
-  
-  attributes(ump)
-  print(ump$layout)
-  attributes(ump$layout)
-  ump$layout
-  ump$layout[1,]
-  ump$layout[1,][1]
-  row.names(ump$layout)
-  
-  library(plotly)
-  i <- 1
-  fig <- plot_ly(type = 'scatter', mode = 'markers')
-  for(row in row.names(ump$layout)){
-  fig <- fig %>% add_trace(x = ump$layout[i,][1], y = ump$layout[i,][2], name = row)
-  i <- i+1
-  }
-  fig
-  warnings()
+  data <- lmFit(naOmitInput)
+  data
