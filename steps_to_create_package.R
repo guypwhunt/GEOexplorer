5ff74d319a2790da234f13ab35029d7eb99dde02
Guy Hunt <guypwhunt1992@gmail.com>
2021-05-23 15:34:53 +0100

    PCA Bug Updates

--- steps_to_create_package.R
+++ steps_to_create_package.R
@@ -43,22 +43,19 @@ geo2rShinyApp::loadApp()
 # Install from GitHub
 install.packages("devtools")
 library(devtools)
+install_github("guypwhunt/r_shiny_geo2r_visulisation", force = TRUE, ref = "main")
+
 
 # Test library
 install_github("guypwhunt/r_shiny_geo2r_visulisation", force = TRUE, ref = "master")
 library(geo2rShinyApp)
+geo2rShinyApp::loadApp()
 help(package = "geo2rShinyApp")
 
 # Test functions
 geoAccessionCode <- "GSE18388"
-logTransformation <- "Auto-Detect"  # Values can also be "Yes" or "No"
-knnTransformation <- "No" # Values can also be "Yes"
-knn <- 2
-pValueAdjustment <- "Benjamini & Hochberg (False discovery rate)"
-limmaPrecisionWeights <- "No"
-forceNormalization <- "No"
-platformAnnotation <- "NCBI generated"
-significanceLevelCutOff <- 0.05
+logTransformation <- "Auto-Detect"
+knnTransformation <- "Yes"
 
 allGset <- getGeoObject(geoAccessionCode)
 platforms <- extractPlatforms(allGset)
